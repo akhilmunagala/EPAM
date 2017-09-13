c36d0ee723ae0ee4347c36968efc9fe99096db01
n3utr0n <akhil.munagala2@gmail.com>
2017-09-13 21:06:32 +0530

    created object

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
-		
+		Student s = new Student(538,"Akhil Munagala",'1996-06-20',61.1);
 
 	}
 
